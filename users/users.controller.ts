7573ff61c836c354f708c24afa3a073dfd8bc6c4
aditya sharma <aditya123_14dec@yahoo.com>
2025-01-08 17:10:26 +0530

    Authentication applied

--- users/users.controller.ts
+++ users/users.controller.ts
@@ -1,19 +1,43 @@
-import { Body , Controller , Post,Get,Patch,Param,Query,Delete, NotFoundException, UseInterceptors} from '@nestjs/common';
+import { UseGuards,Session,Body , Controller , Post,Get,Patch,Param,Query,Delete, NotFoundException, UseInterceptors} from '@nestjs/common';
 import { CreateUserDto } from './create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './update-user.dto';
 import { Serialize, SerializeInterceptor } from 'interceptors/serialize.interceptors';
 import { UserDto } from './user.dto';
+import { AuthService } from './auth.service';
+import { CurrentUser } from '../users/current-user-decorator'
+import { CurrentUserInterceptor } from './current-user.interceptor';
+import { user } from './user.entity'
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Serialize(UserDto)
 @Controller('auth')
+@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
-    constructor(private usersService: UsersService){}
-    @Post('/signup')
-    createUser(@Body() body: CreateUserDto){
-        this.usersService.create(body.email,body.password)
+    constructor(private usersService: UsersService , private authService:AuthService){}
+    @Get('/whoami')
+    @UseGuards(AuthGuard)
+    whoAmI(@CurrentUser() user:user){
+        return user;
     }
     
+    @Post('/signout')
+    signOut(@Session() session: any){
+        session.userId=null;
+    }
+
+    @Post('/signup')
+    async createUser(@Body() body: CreateUserDto,@Session() session:any){
+        const user = await this.authService.signup(body.email,body.password)
+        session.userId = user.id
+        return user;
+    }
+    @Post('/signin')
+    async signin(@Body() body:CreateUserDto,@Session() session:any){
+        const user = await this.authService.signin(body.email,body.password)
+        session.userId = user.id
+        return user;
+    }
     @Get('/:id')
     async findUser(@Param('id') id: string){
         const user = await this.usersService.findOne(parseInt(id))
--- users/users.module.ts
+++ users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { user } from './user.entity'
+import { AuthService } from './auth.service'
+import { CurrentUserInterceptor } from './current-user.interceptor'
 
 @Module({
     imports:[TypeOrmModule.forFeature([user])],
     controllers: [UsersController],
-    providers: [UsersService],
+    providers: [UsersService,AuthService,CurrentUserInterceptor],
 })
 export class UsersModule {}
